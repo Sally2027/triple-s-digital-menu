298e57d2dccb1a1c213526d85581fc3cb880fef8
Sally2027 <sallyalazab123@gmail.com>
2025-05-20 22:18:53 +0000

    working on the website

--- triple-s-digital-menu/assets/js/script.js
+++ triple-s-digital-menu/assets/js/script.js
@@ -7,3 +7,45 @@ $(document).ready(function() {
     dots: true,
   });
 });
+
+$(document).ready(function() {
+  document.getElementById("year").textContent = new Date().getFullYear();
+});
+
+// Back to Top Button
+ 
+ jQuery(document).ready(function($) {
+    var progressPath = document.querySelector('.progress-wrap path');
+    var pathLength = progressPath.getTotalLength();
+
+    progressPath.style.transition = progressPath.style.WebkitTransition = 'none';
+    progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
+    progressPath.style.strokeDashoffset = pathLength;
+    progressPath.getBoundingClientRect();
+    progressPath.style.transition = progressPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';
+
+    var updateProgress = function () {
+        var scroll = $(window).scrollTop();
+        var height = $(document).height() - $(window).height();
+        var progress = pathLength - (scroll * pathLength / height);
+        progressPath.style.strokeDashoffset = progress;
+    };
+
+    updateProgress();
+
+    $(window).scroll(function () {
+        updateProgress();
+        if ($(this).scrollTop() > 50) {
+            $('.progress-wrap').addClass('active-progress');
+        } else {
+            $('.progress-wrap').removeClass('active-progress');
+        }
+    });
+
+    $('.progress-wrap').on('click', function (event) {
+        event.preventDefault();
+        $('html, body').animate({ scrollTop: 0 }, 550);
+        return false;
+    });
+});
+
